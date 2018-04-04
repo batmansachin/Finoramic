ae19ea3c6f98f02a8e69a59f839b80e3e44c485d
avicii <sachin12kumar007@gmail.com>
2018-04-04 14:58:11 +0530

    first commit

---
+++ second2.py
@@ -0,0 +1,44 @@
+import pip
+import subprocess
+import json
+
+def install(name):
+    try:
+        subprocess.call(['pip3', 'install', name])
+        print "Successfully \n"
+    except ImportError:
+    	print "Error in installing\n"
+    	print "Fail in installing "
+
+# Example
+if __name__ == '__main__':
+		
+	Dependencies = {
+		'beautifulsoup4' : '4.4.1',
+		'boto' : '2.48.0',
+		'bz2file' : '0.98',
+		'certifi' : '2017.7.27.1',
+		'chardet' : '3.0.4',
+		'gensim' : '2.3.0',
+		'html5lib' : '0.999',
+		'idna' : '2.5',
+		'nltk' : '3.2.4',
+		'numpy' : '1.13.1',
+		'pexpect' : '4.0.1',
+		'pip' : '9.0.1',
+		'ptyprocess' : '0.5',
+		'pyxdg' : '0.25',
+		'reportlab' : '3.3.0',
+		'requests' : '2.18.3',
+		'scipy' : '0.19.1',
+		'setuptools' : '20.7.0',
+		'six' : '1.10.0',
+		'smart-open' : '1.5.3',
+		'textblob' : '0.12.0',
+		'twitter' : '1.17.1',
+		'urllib3' : '1.22',
+		}
+	
+	for Name in Dependencies:
+		print ("installing "+Name+" library :- \n") 
+		install(Name)
